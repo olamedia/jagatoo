d16def54beb73f7da2006b70e945ddecea85a79e
mkbluesky <mkbluesky@2129d3d7-e42f-0410-990c-ad1eb91dfb1e>
2007-06-09 09:29:24 +0000

    added two groovy jars

--- src/org/jagatoo/scripting/GroovyGUIShellImpl.groovy
+++ src/org/jagatoo/scripting/GroovyGUIShellImpl.groovy
@@ -2,6 +2,7 @@ package org.jagatoo.scripting
 
 import javax.swing.*
 import java.awt.*
+import java.awt.event.*
 
 /**
  * Implementation of a basic groovy shell
@@ -10,17 +11,22 @@ import java.awt.*
 class GroovyShellImpl implements EventListener {
 
   public GroovyShellImpl() {
-      
-      def frame = new JFrame()
-      
-      frame.visible = true
-      frame.size = new Dimension(800,600)
-      frame.keyPressed = {
-          println it
-      }
-      
+
+	def frame = new JFrame()
+
+	frame.visible = true
+	frame.size = new Dimension(800,600)
+	frame.keyPressed = {
+		if(it.keyCode == KeyEvent.VK_ESCAPE) {
+			frame.visible = false
+			System.exit(0)
+		} else {
+		    println("Key event : "+it.keyCode)
+		}
+	}
+
   }
-  
+
   static void main(String[] argv) {}
 
 }
