675da4ec8e0d19531da78173241dc57df78e5046
qudus <qudus@2129d3d7-e42f-0410-990c-ad1eb91dfb1e>
2007-06-24 01:54:34 +0000

    Reverted reformatting.

--- src/org/jagatoo/datatypes/Placeable.java
+++ src/org/jagatoo/datatypes/Placeable.java
@@ -5,59 +5,59 @@ import org.openmali.vecmath.Tuple3f;
 
 /**
  * Placeable object : you can get/set Position and Rotation
- *
+ * 
  * @author Marvin Froehlich (aka Qudus)
  * @author Amos Wenger (aka BlueSky)
  */
 public interface Placeable {
-
+    
     /**
      * @return The position of this Placeable object
      */
     public Tuple3f getPosition();
-
+    
     /**
      * Write the position of this Placeable object to the
      * given Tuple3f
      * @param pos The Tuple3f to put the pos into
      */
     public void getPosition(Tuple3f pos);
-
+    
     /**
      * Set the position of this Placeable object
      * @param pos
      */
     public void setPosition(Tuple3f pos);
-
+    
     /**
      * Set the position of this Placeable object.
      */
     public void setPosition(float posX, float posY, float posZ);
-
+    
     /**
      * @return The rotation, in Euler angles (degrees) of
      * this Placeable object
      */
     public Tuple3f getRotation();
-
+    
     /**
      * Write the rotation, in Euler angles (degrees) of
      * this Placeable object in the given Tuple3f
      */
     public void getRotation(Tuple3f rot);
-
+    
     /**
      * @return The rotation, as a 3x3 rotation Matrix,
      * of this Placeable object
      */
     public Matrix3f getRotationMatrix();
-
+    
     /**
      * Write the rotation, as a 3x3 rotation Matrix,
      * of this Placeable object in the given Matrix3f
      */
     public void getRotationMatrix(Matrix3f rot);
-
+    
     /**
      * Set the rotation of this object, in Euler angles
      * (degrees)
@@ -65,14 +65,14 @@ public interface Placeable {
      * this Placeable object
      */
     public void setRotation(Tuple3f rot);
-
+    
     /**
      * Set the rotation Matrix of this object
      * @param rot The rotation, as a 3x3 rotation Matrix,
      * of this Placeable object
      */
     public void setRotationMatrix(Matrix3f rot);
-
+    
     /**
      * Set the rotation parameter
      * @param rotX The x-rotation of the Collideable
@@ -80,5 +80,5 @@ public interface Placeable {
      * @param rotZ The z-rotation of the Collideable
      */
     public void setRotation(float rotX, float rotY, float rotZ);
-
+    
 }
--- src/org/jagatoo/datatypes/impl/PlaceableImpl.java
+++ src/org/jagatoo/datatypes/impl/PlaceableImpl.java
@@ -43,123 +43,123 @@ import org.openmali.vecmath.Tuple3f;
  * A small, GC-friendly implementation of the Placeable
  * interface. Extend it when you need Placeable without
  * much hassle and you don't already have a superclass.
- *
+ * 
  * @author Amos Wenger (aka BlueSky)
  */
 public abstract class PlaceableImpl implements Placeable {
-
+    
     protected Point3f position;
     protected Matrix3f rotation;
-
+    
     // GC-friendliness
     private static Tuple3f tmpTuple3f = new Point3f();
-
+    
     /**
      * Creation a new PlaceableImpl with pos (0,0,0)
      * and no rotation.
      */
     public PlaceableImpl() {
-
+        
         this.position = new Point3f();
         this.rotation = new Matrix3f();
         this.rotation.setIdentity();
-
+        
     }
-
+    
     public Tuple3f getPosition() {
-
+        
         return this.position;
-
+        
     }
-
+    
     public void getPosition(Tuple3f pos) {
-
+        
         pos.set(this.position);
-
+        
     }
-
+    
     public Tuple3f getRotation() {
-
+        
         MatrixUtils.matrixToEuler(this.rotation, tmpTuple3f);
         return tmpTuple3f;
-
+        
     }
-
+    
     public void getRotation(Tuple3f rot) {
-
+        
         MatrixUtils.matrixToEuler(this.rotation, rot);
-
+        
     }
-
+    
     public Matrix3f getRotationMatrix() {
-
+        
         return this.rotation;
-
+        
     }
-
+    
     public void getRotationMatrix(Matrix3f rot) {
-
+        
         rot.set(this.rotation);
-
+        
     }
-
+    
     public void setPosition(Tuple3f pos) {
-
+        
         this.position.set(pos);
         onPositionChanged();
-
+        
     }
-
+    
     public void setPosition(float posX, float posY, float posZ) {
-
+        
         this.position.set(posX, posY, posZ);
         onPositionChanged();
-
+        
     }
-
+    
     public void setRotation(Tuple3f rot) {
-
+        
         MatrixUtils.eulerToMatrix3f(rot, this.rotation);
         onRotationChanged();
-
+        
     }
-
+    
     public void setRotation(float rotX, float rotY, float rotZ) {
-
+        
         MatrixUtils.eulerToMatrix3f(rotX, rotY, rotZ, this.rotation);
         onRotationChanged();
-
+        
     }
-
+    
     public void setRotationMatrix(Matrix3f rot) {
-
+        
         this.rotation.set(rot);
         onRotationChanged();
-
+        
     }
-
+    
     /*
      * Additionnal methods (e.g. for those wanting to connect
      * Interpolators to only one dimension
      */
-
+    
     public float getPositionX() {return this.position.x;}
     public float getPositionY() {return this.position.y;}
     public float getPositionZ() {return this.position.y;}
-
+    
     public float getRotationX() {return getRotation().x;}
     public float getRotationY() {return getRotation().y;}
     public float getRotationZ() {return getRotation().z;}
-
+    
     public void setPositionX(float v) {this.position.x = v; onPositionChanged();}
     public void setPositionY(float v) {this.position.y = v; onPositionChanged();}
     public void setPositionZ(float v) {this.position.z = v; onPositionChanged();}
-
+    
     public void setRotationX(float v) {Tuple3f rot = getRotation(); MatrixUtils.eulerToMatrix3f(v, rot.y, rot.z, this.rotation); onRotationChanged();}
     public void setRotationY(float v) {Tuple3f rot = getRotation(); MatrixUtils.eulerToMatrix3f(rot.x, v, rot.z, this.rotation); onRotationChanged();}
     public void setRotationZ(float v) {Tuple3f rot = getRotation(); MatrixUtils.eulerToMatrix3f(rot.x, rot.y, v, this.rotation); onRotationChanged();}
-
+    
     public abstract void onPositionChanged();
     public abstract void onRotationChanged();
-
+    
 }
