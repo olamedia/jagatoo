6b9dba61e9a3d318c2c71a5f5b72b510ab228207
mkbluesky <mkbluesky@2129d3d7-e42f-0410-990c-ad1eb91dfb1e>
2007-07-16 13:51:38 +0000

    fixed and improved Javadoc

--- src/org/jagatoo/datatypes/Placeable.java
+++ src/org/jagatoo/datatypes/Placeable.java
@@ -5,59 +5,59 @@ import org.openmali.vecmath.Tuple3f;
 
 /**
  * Placeable object : you can get/set Position and Rotation
- * 
+ *
  * @author Marvin Froehlich (aka Qudus)
  * @author Amos Wenger (aka BlueSky)
  */
 public interface Placeable {
-    
+
     /**
      * @return The position of this Placeable object
      */
     public Tuple3f getPosition();
-    
+
     /**
      * Write the position of this Placeable object to the
      * given Tuple3f
      * @param pos The Tuple3f to put the pos into
      */
     public void getPosition(Tuple3f pos);
-    
+
     /**
      * Set the position of this Placeable object
      * @param pos
      */
     public void setPosition(Tuple3f pos);
-    
+
     /**
      * Set the position of this Placeable object.
      */
     public void setPosition(float posX, float posY, float posZ);
-    
+
     /**
      * @return The rotation, in Euler angles (degrees) of
      * this Placeable object
      */
     public Tuple3f getRotation();
-    
+
     /**
      * Write the rotation, in Euler angles (degrees) of
      * this Placeable object in the given Tuple3f
      */
     public void getRotation(Tuple3f rot);
-    
+
     /**
      * @return The rotation, as a 3x3 rotation Matrix,
      * of this Placeable object
      */
     public Matrix3f getRotationMatrix();
-    
+
     /**
      * Write the rotation, as a 3x3 rotation Matrix,
      * of this Placeable object in the given Matrix3f
      */
     public void getRotationMatrix(Matrix3f rot);
-    
+
     /**
      * Set the rotation of this object, in Euler angles
      * (degrees)
@@ -65,20 +65,20 @@ public interface Placeable {
      * this Placeable object
      */
     public void setRotation(Tuple3f rot);
-    
+
     /**
      * Set the rotation Matrix of this object
      * @param rot The rotation, as a 3x3 rotation Matrix,
      * of this Placeable object
      */
     public void setRotationMatrix(Matrix3f rot);
-    
+
     /**
-     * Set the rotation parameter
-     * @param rotX The x-rotation of the Collideable
-     * @param rotY The y-rotation of the Collideable
-     * @param rotZ The z-rotation of the Collideable
+     * Set the rotation of this object, in Euler angles
+     * @param rotX The x-rotation of this object
+     * @param rotY The y-rotation of this object
+     * @param rotZ The z-rotation of this object
      */
     public void setRotation(float rotX, float rotY, float rotZ);
-    
+
 }
