72d87baf337221851a6a894a1c193c78f45ca9df
maguila <maguila@2129d3d7-e42f-0410-990c-ad1eb91dfb1e>
2007-09-28 00:02:50 +0000

    The Influece class I`ve forgot to commit

---
+++ src/org/jagatoo/loaders/models/collada/datastructs/controllers/Influence.java
@@ -0,0 +1,21 @@
+package org.jagatoo.loaders.models.collada.datastructs.controllers;
+
+/**
+ *  Easy manipulation for weight value of one bone, for a vertex
+ *  
+ * @author Matias Leone (aka Maguila)
+ * @author Amos Wenger (aka BlueSky)
+ */
+public class Influence {
+
+	/**
+	 * Bone index
+	 */
+	public int bone;
+	
+	/**
+	 * Weight value
+	 */
+	public float weight;
+	
+}
