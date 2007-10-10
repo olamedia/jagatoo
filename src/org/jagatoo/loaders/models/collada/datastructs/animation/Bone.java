5e9d2a56f913d7341c772e39b4ed70df7496d7ac
mkbluesky <mkbluesky@2129d3d7-e42f-0410-990c-ad1eb91dfb1e>
2007-10-10 13:27:15 +0000

    changed classpath, removed useless comments in Bone/Skeleton

--- src/org/jagatoo/loaders/models/collada/datastructs/animation/Bone.java
+++ src/org/jagatoo/loaders/models/collada/datastructs/animation/Bone.java
@@ -9,57 +9,42 @@ import org.openmali.vecmath2.Quaternion4f;
 
 /**
  * A Bone
- * 
+ *
  * @author Amos Wenger (aka BlueSky)
  * @author Matias Leone (aka Maguila)
  */
 public class Bone {
-    
+
     /** Children (optional) */
     private List<Bone> children;
-    
+
     /** The name of this bone */
     String name;
-    
+
     /** The bind rotation */
     private final Quaternion4f bindRotation;
-    
+
     /** The length of this bone */
     private float length;
-    
+
     /** The bind matrix */
     final Matrix4f bindMatrix;
-    
+
     /** The inverse bind matrix */
     final Matrix4f invBindMatrix;
-    
+
     /** The rotation of this bone */
     public Quaternion4f relativeRotation;
-    
-    /**
-     * The translation of this bone
-     */
-    /*
-     * Amos Wenger : Only the _ROOT BONE_ can be translated, not the others..
-     * otherwise bones would be no longer attached one to the other. So in fact,
-     * Bones can only scale and rotate. But combining a rotation and a scale
-     * along the Z axis, you can achieve a translation. Well, you don't need a
-     * relativeTranslation at all : you will have "translation keyframes" only
-     * for the root bone, and we can handle that with the "Transform" object in
-     * our "Model". OK ?
-     * 
-     * Matias Leone: yes, you are right. No relativeTranslation.
-     */
-    // public Point3f relativeTranslation;
+
     /**
      * The scaling of this bone, along the three axis : X, Y, and Z.
      */
     public Point3f relativeScaling;
-    
+
     /**
      * The absolute rotation of this bone, ie, a multiplication of all the
      * velocities from the rootBone to this bone, passing by its parents.
-     * 
+     *
      * Stupid question #1 : Why isn't it automatically updated when I
      * setRotation() ? Intelligent answer #1 : For efficiency. If you set the
      * rotation of several bones in a skeleton, you want it only to recompute
@@ -67,10 +52,10 @@ public class Bone {
      * setRotation()
      */
     final Quaternion4f absoluteRotation;
-    
+
     /**
      * The absolute translation of this bone, ie, the position of this bone...
-     * 
+     *
      * Stupid question #1 bis : Why isn't it automatically updated when I
      * setRotation() ? Intelligent answer #1 bis : For efficiency. If you set
      * the rotation of several bones in a skeleton, you want it only to
@@ -78,21 +63,13 @@ public class Bone {
      * setRotation()
      */
     final Point3f absoluteTranslation;
-    
+
     /**
      * The absolute scaling of this bone, along the three axis : X, Y, and Z.
      */
     public Point3f absoluteScaling;
-    
 
-    /*
-     * Amos Wenger : again, we would rather store that in the Skeleton itself,
-     * because there can be (not mandatory) translation keyframes ONLY for the
-     * root bone.
-     * 
-     * Matias Leone: ok.
-     */
-    // public KeyFrame[] transKeyFrames;
+
     /**
      * Temporal key frames for the bone. They are reference extracted from the
      * current animation and they will change every time you play a different
@@ -107,11 +84,11 @@ public class Bone {
      * These are SCALE key frames.
      */
     public KeyFramePoint3f[] scaleKeyFrames;
-    
-    
+
+
     /**
      * Create a new Bone
-     * 
+     *
      * @param name
      *                The name of this bone
      * @param length
@@ -120,107 +97,106 @@ public class Bone {
      *                The bind rotation of this bone
      */
     public Bone(String name, float length, Quaternion4f bindRotation) {
-        
+
         this.name = name;
-        
+
         this.length = length;
         this.bindRotation = bindRotation;
-        
+
         this.bindMatrix = new Matrix4f();
         this.bindMatrix.set(bindRotation);
         this.bindMatrix.m23( length ); // Set the Z translational component of
                                         // the matrix to length
         this.invBindMatrix = new Matrix4f();
         invBindMatrix.invert(bindMatrix);
-        
+
+        // Init relative rot/scaling
         this.relativeRotation = new Quaternion4f(0f, 0f, 0f, 1f);
-        /*
-         * Here, no need either.
-         */
-        // this.relativeTranslation = new Point3f( 0f, 0f, 0f );
+        this.relativeScaling = new Point3f(1f, 1f, 1f);
+
         // Init absolute rot/trans/scaling
         this.absoluteRotation = new Quaternion4f(0f, 0f, 0f, 1f);
         this.absoluteTranslation = new Point3f(0f, 0f, 0f);
         this.absoluteScaling = new Point3f(1f, 1f, 1f);
-        
+
     }
-    
+
     /**
      * Add a child bone
-     * 
+     *
      * @param bone
      */
     public void addChild(Bone bone) {
-        
+
         if (children == null) {
             children = new ArrayList<Bone>();
         }
         children.add(bone);
-        
+
     }
-    
+
     /**
      * Remove a child bone
-     * 
+     *
      * @param bone
      */
     public void remove(Bone bone) {
-        
+
         if (children != null) {
             children.remove(bone);
         }
-        
+
     }
-    
+
     /**
      * @return the number of children of this bone
      */
     public int numChildren() {
-        
+
         return (children == null) ? 0 : children.size();
-        
+
     }
-    
+
     /**
      * Get a bone per index.
-     * 
+     *
      * @param i
      *                The index of the bone you want to get.
      * @return The bone :)
      */
     public Bone getChild(int i) {
-        
+
         return (children == null) ? null : children.get(i);
-        
+
     }
-    
+
     /**
      * @return the bindRotation
      */
     public Quaternion4f getBindRotation() {
         return bindRotation;
     }
-    
+
     /**
      * Set the bindRotation
-     * 
+     *
      * @param quat
      */
     public void setBindRotation(Quaternion4f quat) {
         bindRotation.set(quat);
         bindMatrix.set(bindRotation);
     }
-    
+
     /**
      * @return the length
      */
     public float getLength() {
         return length;
     }
-    
+
     /**
      * Set the length
-     * 
+     *
      * @param length
      */
     public void setLength(float length) {
@@ -228,117 +204,86 @@ public class Bone {
         bindMatrix.m23( length );
         invBindMatrix.invert(bindMatrix);
     }
-    
+
     @Override
     public String toString() {
-        
+
         return name + "] Bind rotation : " + bindRotation + ", length : "
                 + length;
-        
+
     }
-    
+
     /**
      * @return the name of this bone
      */
     public String getName() {
-        
+
         return name;
-        
+
     }
-    
+
     /**
      * Set the name of this bone
-     * 
+     *
      * @param name
      */
     public void setName(String name) {
-        
+
         this.name = name;
-        
+
     }
-    
+
     /**
      * Get the absolute rotation of this bone. Absolute translation/rotation are
      * updated by the Skeleton.updateAbsolutes() method.
-     * 
+     *
      * @return a quaternion containing the absolute rotation
      */
     public Quaternion4f getAbsoluteRotation() {
         return absoluteRotation;
     }
-    
+
     /**
      * Get the absolute translation of this bone. Absolute translation/rotation
      * are updated by the Skeleton.updateAbsolutes() method.
-     * 
+     *
      * @return a point3f containing the absolute translation
      */
     public Point3f getAbsoluteTranslation() {
         return absoluteTranslation;
     }
-    
-    
+
+
     /**
      * @return true if the bone has at least one key frame of any kind
      */
     public boolean hasKeyFrames() {
-        return /* transKeyFrames.length + */rotKeyFrames.length
+        return rotKeyFrames.length
                 + scaleKeyFrames.length > 0;
     }
-    
+
     /**
      * Completes the relativeTranslation and relativeRotation all with 0.
      */
     public void setNoRelativeMovement() {
-        // this.relativeTranslation.set(0f, 0f, 0f);
-        /*
-         * Amos Wenger : there was a mistake here. You set the relativeRotation
-         * to (0f, 0f, 0f, 0f), which is the "addition identity quaternion",
-         * which we do not want. We want the "multiplication identity
-         * quaternion", which is (0f, 0f, 0f, 1f)
-         */
         this.relativeRotation.set(0f, 0f, 0f, 1f);
         this.relativeScaling.set(0f, 0f, 0f);
     }
-    
-    /*
-     * Amos Wenger : Again, this has been moved in Skeleton, as concerns only
-     * the root bone.
-     * 
-     * Matias Leone: ok
-     */
-    /**
-     * Selects the current translation key frame, based on the current time
-     * 
-     * @param currenTime
-     *                beetween 0 and the end of the animation, in miliseconds
-     * @return frame index selected
-     */
-    /*
-     * public int selectCurrentTransFrame(long currentTime) { return
-     * searchNextFrame( transKeyFrames, currentTime ); }
-     */
 
     /**
      * Selects the current translation key frame, based on the current time
-     * 
+     *
      * @param currentTime
      *                beetween 0 and the end of the animation, in miliseconds
      * @return frame index selected
      */
     public int selectCurrentRotFrame(long currentTime) {
-        /*
-         * Amos Wenger : there was a mistake here. You used "transKeyFrames"
-         * instead of "rotKeyFrames"
-         * 
-         * Matias Leone: sorry, copy paste's fault
-         */
         return KeyFrame.searchNextFrame(rotKeyFrames, currentTime);
     }
-    
+
     /**
      * Selects the current scaling key frame, based on the current time
-     * 
+     *
      * @param currentTime
      *                beetween 0 and the end of the animation, in miliseconds
      * @return frame index selected
@@ -346,7 +291,5 @@ public class Bone {
     public int selectCurrentScaleFrame(long currentTime) {
         return KeyFrame.searchNextFrame(scaleKeyFrames, currentTime);
     }
-    
-
 
 }
--- src/org/jagatoo/loaders/models/collada/datastructs/animation/Skeleton.java
+++ src/org/jagatoo/loaders/models/collada/datastructs/animation/Skeleton.java
@@ -7,61 +7,61 @@ import org.openmali.vecmath2.Point3f;
 
 /**
  * A Skeleton. It contains a root bone, which can have several children
- * 
+ *
  * @author Amos Wenger (aka BlueSky)
  * @author Matias Leone (aka Maguila)
  */
 public class Skeleton implements Iterable<Bone> {
-    
+
     /** The root bone : there's only one (for simple implementation) */
     public final Bone rootBone;
-    
+
     // GC-friendly hacks
     private final static Matrix3f tempMatrix = new Matrix3f();
     private final static Point3f tempPoint = new Point3f();
-    
+
     /**
      * The position of our Skeleton. The root bone is transformed by this
      * position (and thus, the children bones are transformed too).
      */
     public final Point3f relativeTranslation;
-    
+
     /** The keyframes of position */
     public KeyFramePoint3f[] transKeyFrames;
-    
+
     /**
      * Iterator for easy managment of the bones
      */
     private SkeletonIterator iterator;
-    
-    
+
+
     /**
      * Create a new Skeleton
-     * 
+     *
      * @param rootBone
      *                The root bone
      * @param relativeTranslation
      *                The position of the Skeleton
      */
     public Skeleton(Bone rootBone, Point3f relativeTranslation) {
-        
+
         this.rootBone = rootBone;
         this.relativeTranslation = relativeTranslation;
-        
+
     }
-    
+
     /**
      * @return the rootBone
      */
     public Bone getRootBone() {
-        
+
         return rootBone;
-        
+
     }
-    
+
     /**
      * Selects the current translation key frame, based on the current time
-     * 
+     *
      * @param currentTime
      *                beetween 0 and the end of the animation, in miliseconds
      * @return frame index selected
@@ -69,48 +69,42 @@ public class Skeleton implements Iterable<Bone> {
     public int selectCurrentTransFrame(long currentTime) {
         return KeyFrame.searchNextFrame(transKeyFrames, currentTime);
     }
-    
+
     /**
      * Update the skeleton. It recomputes the transform matrix of each bone,
      * starting with the root bone and then each child recursively.
      */
     public void updateAbsolutes() {
-        
+
         updateBone(null, rootBone);
-        
+
     }
-    
+
     /**
      * Update a bone, e.g. compute its absolute rotation/translation from its
      * relative ones.
-     * 
+     *
      * @param parentTrans
      * @param bone
      */
     private void updateBone(Bone parentBone, Bone bone) {
-        
+
         if (parentBone == null) {
-            
+
             /*
              * Root bone
              */
 
             bone.absoluteRotation.set(bone.getBindRotation());
             bone.absoluteRotation.mul(bone.relativeRotation);
-            
+
             bone.absoluteTranslation.set(relativeTranslation);
             bone.absoluteTranslation.addZ( bone.getLength() );
-            
+
             bone.absoluteScaling.set(bone.relativeScaling);
-            
-            /*
-             * Amos Wenger : nope, relativeTranslation is used only for the root
-             * bone.
-             */
-            // FIXME need to use relativeTranslation and relativeScaling
-            
+
         } else {
-            
+
             /*
              * Child bone
              */
@@ -118,46 +112,40 @@ public class Skeleton implements Iterable<Bone> {
             bone.absoluteRotation.set(parentBone.absoluteRotation);
             bone.absoluteRotation.mul(bone.getBindRotation());
             bone.absoluteRotation.mul(bone.relativeRotation);
-            
+
             bone.absoluteTranslation.set(parentBone.absoluteTranslation);
             tempMatrix.set(bone.absoluteRotation);
             tempPoint.set(0f, 0f, bone.getLength());
             tempMatrix.transform(tempPoint);
             bone.absoluteTranslation.add(tempPoint);
-            
+
             bone.absoluteScaling.set(parentBone.absoluteScaling);
             bone.absoluteScaling.set(bone.absoluteScaling.getX()
                     * bone.relativeScaling.getX(), bone.absoluteScaling.getY()
                     * bone.relativeScaling.getY(), bone.absoluteScaling.getZ()
                     * bone.relativeScaling.getZ());
-            
-            /*
-             * Amos Wenger : nope, relativeTranslation is used only for the root
-             * bone
-             */
-            // FIXME need to use relativeTranslation and relativeScaling
-            
+
         }
-        
+
         // Both for root and children nodes
         bone.absoluteTranslation.set(bone.absoluteTranslation.getX()
                 * bone.absoluteScaling.getX(), bone.absoluteTranslation.getY()
                 * bone.absoluteScaling.getY(), bone.absoluteTranslation.getZ()
                 * bone.absoluteScaling.getZ());
-        
+
         for (int i = 0; i < bone.numChildren(); i++) {
-            
+
             updateBone(bone, bone.getChild(i));
-            
+
         }
-        
+
     }
-    
+
     public Iterator<Bone> iterator() {
         if (this.iterator == null) {
             this.iterator = new SkeletonIterator(this);
         }
         return this.iterator;
     }
-    
+
 }
