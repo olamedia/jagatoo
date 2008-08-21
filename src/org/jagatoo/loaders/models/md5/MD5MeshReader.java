79b822bb651e45e49f82cde7d477fad6a77508f7
qudus <qudus@2129d3d7-e42f-0410-990c-ad1eb91dfb1e>
2008-08-21 19:44:04 +0000

    Applied some basic JavaDoc to the MD5 loader.

--- src/org/jagatoo/loaders/models/md5/MD5MeshReader.java
+++ src/org/jagatoo/loaders/models/md5/MD5MeshReader.java
@@ -27,37 +27,6 @@
  * RISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE
  */
-/**
- * Copyright (c) 2006 KProject
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- * * Redistributions of source code must retain the above copyright
- *   notice, this list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright
- *   notice, this list of conditions and the following disclaimer in the
- *   documentation and/or other materials provided with the distribution.
- *
- * * Neither the name of 'KProject' nor the names of its contributors 
- *   may be used to endorse or promote products derived from this software 
- *   without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package org.jagatoo.loaders.models.md5;
 
 import org.jagatoo.datatypes.NamedObject;
@@ -87,7 +56,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * @author kman
+ * Loads MD5MESH files and pushes them through JAGaToo's factory interfaces
+ * to generate model data on the implementing scenegraph's side.
+ * 
+ * Some basic ideas are taken from The kman's MD5 loader.
+ * 
  * @author Marvin Froehlich (aka Qudus)
  */
 public class MD5MeshReader
